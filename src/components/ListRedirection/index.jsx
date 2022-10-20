8046c0e60cf86eff721cd85df1a63e166d942ba8
Yubor Molina <yubor.molina@onthefuze.com.au>
2022-10-20 15:09:45 -0500

    correcting links 2

--- src/components/ListRedirection/index.jsx
+++ src/components/ListRedirection/index.jsx
@@ -22,7 +22,7 @@ const ListRedirection = () =>  {
     return(
         <div className="ListRedirection">
 
-            <Link to="/"><a className="btn btn-primary mb-4 ">Go Back</a></Link>
+            <Link to="/giphy-api"><a className="btn btn-primary mb-4 ">Go Back</a></Link>
             <div className="ListRedirection-container" >
                 {
                     gifs.map(gif => <ImageGif key={gif.images.original.url} src={gif.images.original.url} href={`/giphy-api/detail/${gif.id}`}/>)
